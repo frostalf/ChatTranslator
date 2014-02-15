45651c6922839b5684006b48a001e92186a38c1c
frostalf <frostalf@gmail.com>
2014-02-15 04:38:05 -0800

    Added conversation API, still needs a bit of code to complete.

--- src/main/java/net/frostalf/translate/commands/TranslateEnable.java
+++ src/main/java/net/frostalf/translate/commands/TranslateEnable.java
@@ -1,9 +1,16 @@
 
 package net.frostalf.translate.commands;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.frostalf.translate.Translate;
+import net.frostalf.translate.database.ConnectionPool;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  *
@@ -11,9 +18,33 @@ import org.bukkit.command.CommandSender;
  */
 public class TranslateEnable implements CommandExecutor {
     
+    private Translate plugin;
+    
+    public TranslateEnable (Translate plugin) {
+        this.plugin = plugin;
+    }
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            ConnectionPool connectionPool = new ConnectionPool(plugin);
+            if(cmd.getName().equalsIgnoreCase("TranslateEnable")) {
+                if(!connectionPool.getConnection()) {
+                    connectionPool.connection();
+                }
+                try {
+                    Statement stmt = connectionPool.connection().createStatement();
+                    String query = "UPDATE translate set enabled = 1 where playername=" + player.getName();
+                    stmt.executeUpdate(query);
+                    stmt.close();                    
+                } catch (SQLException ex) {
+                    Logger.getLogger(TranslateEnable.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+        }
         return true;
     }
 
---
+++ src/main/java/net/frostalf/translate/util/Conversation.java
@@ -0,0 +1,127 @@
+
+package net.frostalf.translate.util;
+
+import org.bukkit.conversations.ConversationContext;
+import net.frostalf.translate.Translate;
+import org.bukkit.ChatColor;
+import org.bukkit.conversations.ConversationFactory;
+import org.bukkit.conversations.MessagePrompt;
+import org.bukkit.conversations.Prompt;
+import org.bukkit.conversations.StringPrompt;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Frostalf
+ */
+public class Conversation {
+    
+    private Translate plugin;
+    private ConversationFactory factory;
+    private String playerName;
+    private boolean enabled;
+    private String languageCode;
+    private Player player;
+    
+    public Conversation (Translate plugin) {
+        this.plugin = plugin;
+    }
+    
+    public void addPlayerDB(Player player, String playerName) {
+        this.player = player;
+        this.playerName = player.getName();
+        this.enabled = false;
+        this.languageCode = null;
+        this.factory = new ConversationFactory(plugin).withModality(true).withFirstPrompt(new Welcome()).withEscapeSequence("/quit").withTimeout(120).thatExcludesNonPlayersWithMessage("Must be a Player!");
+        factory.buildConversation(player).begin();
+        
+    }
+    
+    private class Welcome extends MessagePrompt {
+        
+        @Override
+        public String getPromptText(ConversationContext context) {
+            context.setSessionData("playerName", playerName);
+            return ChatColor.translateAlternateColorCodes('&', "&2This Server uses a Language translator.\nThese next questions will guide you through asking which language preference you prefer\nand whether or not you want it enabled/disabled.\nYou can disable/enable later as well.");
+        }
+        
+        @Override
+        protected Prompt getNextPrompt(ConversationContext context) {
+            return new chooseLanguage();
+        }
+        
+    }
+    
+    private class chooseLanguage extends StringPrompt {
+        @Override
+        public String getPromptText(ConversationContext context) {
+            return ChatColor.translateAlternateColorCodes('&', "&8>&7First, set your two character language code.\n&7Type a 2 character language code to continue!");
+        }
+
+        @Override
+        public Prompt acceptInput(ConversationContext context, String input) {
+            if (input.length() == 2) {
+                languageCode = input.toUpperCase();
+                context.setSessionData("languagecode", languageCode);
+                return new enable();
+            } else {
+                return new chooseLanguage();
+            }
+        }
+    }
+    
+    private class enable extends StringPrompt {
+        
+        @Override
+        public String getPromptText(ConversationContext context) {
+            return ChatColor.translateAlternateColorCodes('&', "&8>&7Would you like to enable the translator?\n&7Type yes to enable or no to not enalbe.\nYou can enable translator later with a command or disable");
+        }
+        
+        @Override
+        public Prompt acceptInput(ConversationContext context, String input) {
+            if(input.equalsIgnoreCase("yes")){
+                enabled = true;
+                context.setSessionData("enabled", enabled);
+                return new endMessage();
+            } else if(input.equalsIgnoreCase("no")) {
+                enabled = false;
+                context.setSessionData("enabled", enabled);
+                return new endMessage();
+            }
+            return new enable();
+        }
+        
+    }
+    
+    private class endMessage extends StringPrompt {
+        
+        @Override
+        public String getPromptText(ConversationContext context) {
+            return ChatColor.translateAlternateColorCodes('&', "&2Is this correct?\nLanguage Code: " + context.getSessionData("languagecode").toString() + "\nEnabled: " + context.getSessionData("enabled").toString());
+        }
+        
+        @Override
+        public Prompt acceptInput (ConversationContext context, String input) {
+            if(input.equalsIgnoreCase("yes")) {
+                return new finishMessage();
+            } else if (input.equalsIgnoreCase("no")) {
+                return new chooseLanguage();
+            }
+            return new endMessage();
+        }
+    }
+    
+    private class finishMessage extends MessagePrompt {
+        
+        @Override
+        public String getPromptText(ConversationContext context) {
+            return ChatColor.translateAlternateColorCodes('&', "&2Thank You!\nEnjoy translated messages if you chose to enable it!");
+        }
+        
+        @Override
+        public Prompt getNextPrompt(ConversationContext context) {
+            return END_OF_CONVERSATION;
+        }
+    }
+
+}
